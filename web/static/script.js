159ac94c24bd2efb135cc2375893f9a5947876d1
Nguyễn Huy Nghĩa <116424780+trongnghia2007@users.noreply.github.com>
2024-06-29 12:24:32 +0700

    Add files via upload

--- web/static/script.js
+++ web/static/script.js
@@ -1,7 +1,7 @@
 console.log("Script loaded successfully!");
 
-// My form
 document.addEventListener("DOMContentLoaded", () => {
+  // My form
   document
     .getElementById("myForm")
     .addEventListener("use-button", function (e) {
@@ -27,9 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Get DOM elements
   const uploadInput = document.getElementById("upload-input");
   const preview = document.getElementById("preview");
-  const myForm = document.getElementById("myForm");
   const actionButtons = document.getElementById("action-buttons");
-  const cancelButton = document.getElementById("cancel-button");
   const useButton = document.getElementById("use-button");
   const outputSection = document.getElementById("output");
 
@@ -70,7 +68,43 @@ document.addEventListener("DOMContentLoaded", () => {
       body: data,
     });
 
-    
+    if (response.ok) {
+      const result = await response.json();
+      try {
+        // remove loading animation
+        document.querySelector("#loading-overlay").style.display = "none";
+        document.body.classList.remove("loading");
+
+        outputSection.style.display = "block"; // Hiển thị phần kết quả
+
+        const frame = document.querySelector("#output");
+        frame.innerHTML = "<h1>Kết Quả</h1>"; // Xóa bỏ nội dung kết quả trước đó
+
+        for (let i of result) {
+          let img = `<div style="margin: 0 auto; width: 224px; height: 224pxborder: 2px solid black">
+            <img src="/images/${i.segment_data}.jpg" style="width:100%; border: 1px solid black">
+            <p style="margin-top: 1rem; margin-bottom: 0px; font-size: 1.25rem">${i.prediction}</p>
+            <p style="margin-top: 0px; font-size: 1.25rem">chính xác: ${i.confidence}%</p>
+          </div>`;
+          frame.innerHTML += img;
+        }
+
+        // Thay thế ảnh preview ban đầu bằng ảnh thực hiện SLIC
+        const slicImage = document.createElement("img");
+        slicImage.src = "/images/.superpixels.jpg"; // Thay đổi đường dẫn tới ảnh SLIC của bạn
+        slicImage.style.maxWidth = "100%";
+        slicImage.style.maxHeight = "100%";
+        preview.innerHTML = "";
+        preview.appendChild(slicImage);
+      } catch(err) {
+        console.error(err); // showing errors
+        outputSection.style.display = "block"; // Hiển thị phần kết quả
+        outputSection.innerHTML = `<h1 style="font-size: 60px">Lỗi</h1><p>${err}</p>`;
+      }
+
+    } else {
+      console.error("Error performing skin recognition");
+    }
   }
 
   // Listen to the "change" event on the file input and call the handleImageUpload function
@@ -95,8 +129,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     var selectedChoices = Array.from(selectedChoicesSet); // Convert set to array
 
-    // document.getElementById('result').textContent = "Selected choices: " + selectedChoices.join(", ");
-
     // create a formData object
     const formData = new FormData();
 
@@ -119,14 +151,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Chat box
-/* 
-function openChat() {
-    alert('Chat icon clicked!');
-    // Thay thế alert bằng hành động thực tế, ví dụ: mở cửa sổ chat
-}
-    */
 
+// Chat box
 document.addEventListener("DOMContentLoaded", function () {
   const chatCircle = document.getElementById("chat-icon");
   const chatBox = document.getElementById("chat-box");
